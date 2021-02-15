62394d6816aa433307fe3036513363d0e6a430e5
hyo1365 <ddj03329@gmail.com>
2021-02-15 16:37:45 +0900

    첫 커밋

---
+++ src/main/java/com/hyo/untactStar/UntactStarApplication.java
@@ -0,0 +1,13 @@
+package com.hyo.untactStar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UntactStarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UntactStarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyo/untactStar/UntactStarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyo.untactStar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UntactStarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
